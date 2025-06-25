1f5db0a425e75f0f5da8e9c0023ad0234a310794
Lakshmipathigoud <gownilakshmipathi@gmail.com>
2025-06-25 11:56:11 +0530

    Add files via upload

---
+++ task1.sql
@@ -0,0 +1,29 @@
+create database pathi;
+use pathi;
+create table Authors(
+A_id int primary key,
+A_name varchar(10),
+A_book varchar(20)
+);
+select * from Authors;
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    quantity INT,
+    price INT,
+    A_id INT,  -- Define the column first
+    FOREIGN KEY (A_id) REFERENCES Authors(A_id)
+);
+
+INSERT INTO Authors (A_id, A_name, A_book) VALUES (101, 'jhon', 'Data Science'),
+                                                   (102, 'Bob', 'AI Basics'),
+												   (103, 'Clara', 'SQL Mastery'),
+                                                    (104, 'David', 'Python 101'),
+                                                     (105, 'Eva', 'Machine Learning');
+                                   
+INSERT INTO Orders (order_id, quantity, A_id, price) VALUES
+(201, 2, 101, 500),
+(202, 1, 102, 300),
+(203, 4, 103, 1200),
+(204, 3, 104, 900),
+(205, 2, 105, 1000);
+               
